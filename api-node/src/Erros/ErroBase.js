64ec91d50d733785d6c54bbf5866977da4092c18
LucasOMeira <meiralucas89@gmail.com>
2023-05-24 15:18:43 -0300

    classe ErroBase

---
+++ api-node/src/Erros/ErroBase.js
@@ -0,0 +1,9 @@
+class ErroBase extends Error {
+  constructor(mensagem = "Erro interno do servidor", status = 500) {
+    super();
+    this.message = mensagem;
+    this.status = status;
+  }
+}
+
+export default ErroBase;
